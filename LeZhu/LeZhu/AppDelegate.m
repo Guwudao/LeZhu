a8eff46517440d9d9bcfb0438dded86d1186d9ff
Jackie <13265466114@163.com>
2018-05-31 17:13:00 +0800

    集成极光推送

--- LeZhu/LeZhu/AppDelegate.m
+++ LeZhu/LeZhu/AppDelegate.m
@@ -11,14 +11,16 @@
 #import <UMSocialCore/UMSocialCore.h>
 #import "GYNewFeatureViewController.h"
 #import "WXApi.h"
+#import "JPUSHService.h"
 
-@interface AppDelegate ()<WXApiDelegate>
-
+@interface AppDelegate ()<WXApiDelegate, JPUSHRegisterDelegate>
+//@property(nonatomic, copy) NSString *appkey
 @end
 
+static NSString *appKey = @"aa7182c25b684f681cbb1f79";
+static NSString *channel = @"App Store";
+
 @implementation AppDelegate
-
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
@@ -37,15 +39,39 @@
     // 初始化蓝牙
 //    [LibDevModel initBluetooth];
 
-
     //创建窗口并显示
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    
     [self chooseRootController];
-    
     [self.window makeKeyAndVisible];
     
+    //集成极光推送
+    [self JPushRegister];
+    // Required
+    // init Push
+    // notice: 2.1.5版本的SDK新增的注册方法，改成可上报IDFA，如果没有使用IDFA直接传nil
+    // 如需继续使用pushConfig.plist文件声明appKey等配置内容，请依旧使用[JPUSHService setupWithOption:launchOptions]方式初始化。
+    [JPUSHService setupWithOption:launchOptions appKey:appKey
+                          channel:channel
+                 apsForProduction:YES
+            advertisingIdentifier:nil];
+    
+    
     return YES;
+    
+}
+
+-(void)JPushRegister{
+    //Required
+    //notice: 3.0.0及以后版本注册可以这样写，也可以继续用之前的注册方式
+    JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
+    entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound;
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+        // 可以添加自定义categories
+        // NSSet<UNNotificationCategory *> *categories for iOS10 or later
+        // NSSet<UIUserNotificationCategory *> *categories for iOS8 and iOS9
+    }
+    [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
+    
 }
 
 
@@ -179,6 +205,50 @@
     //创建tabBar控制器
     GYTabBarViewController *tabVC = [[GYTabBarViewController alloc] init];
     self.window.rootViewController = tabVC;
+}
+
+#pragma mark- JPUSHRegisterDelegate
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    /// Required - 注册 DeviceToken
+    [JPUSHService registerDeviceToken:deviceToken];
+}
+
+
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler {
+    GYLog(@"收到推送");
+    
+    // Required
+//    NSDictionary * userInfo = response.notification.request.content.userInfo;
+//    if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+//        [JPUSHService handleRemoteNotification:userInfo];
+//    }
+//    completionHandler();  // 系统要求执行这个方法
+}
+
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler {
+    GYLog(@"跳转至推送");
+    
+    // Required
+//    NSDictionary * userInfo = notification.request.content.userInfo;
+//    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+//        [JPUSHService handleRemoteNotification:userInfo];
+//    }
+//    completionHandler(UNNotificationPresentationOptionAlert); // 需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以选择设置
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    
+    // Required, iOS 7 Support
+    [JPUSHService handleRemoteNotification:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    
+    // Required,For systems with less than or equal to iOS6
+    [JPUSHService handleRemoteNotification:userInfo];
 }
 
 @end
